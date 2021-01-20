d93124c0c8961ec19ed92b6ce30117a1b68c7761
Lucas-64 <77701545+Lucas-64@users.noreply.github.com>
2021-01-19 22:06:28 -0300

    chat in real time
    chat em tempo real usando socket.io e node

---
+++ chat/public/script.js
@@ -0,0 +1,42 @@
+function backgroundRed() {
+    document.getElementById("backcolor").setAttribute("class", "corRed")
+    document.getElementById("selectColorButton").setAttribute("class", "boxColorRed")
+    document.getElementById("selectImageButton").setAttribute("class", "imgBackRed")
+    document.getElementById("imgColor").setAttribute("class", "boxImgRed")
+    document.getElementById("msg").setAttribute("class", "textareaMsgRed")
+    document.getElementById("Input").setAttribute("class", "inputNameRed")
+    document.getElementById("nChat").setAttribute("class", "chatRed")
+}
+function backgroundBlue() {
+    document.getElementById("backcolor").setAttribute("class", "corBlue")
+    document.getElementById("selectColorButton").setAttribute("class", "boxColorBlue")
+    document.getElementById("selectImageButton").setAttribute("class", "imgBackBlue")
+    document.getElementById("imgColor").setAttribute("class", "boxImgBlue")
+    document.getElementById("msg").setAttribute("class", "textareaMsgBlue")
+    document.getElementById("Input").setAttribute("class", "inputNameBlue")
+    document.getElementById("nChat").setAttribute("class", "chatBlue")
+}
+function backgroundGreen() {
+    document.getElementById("backcolor").setAttribute("class", "corGreen")
+    document.getElementById("selectColorButton").setAttribute("class", "boxColorGreen")
+    document.getElementById("selectImageButton").setAttribute("class", "imgBackGreen")
+    document.getElementById("imgColor").setAttribute("class", "boxImgGreen")
+    document.getElementById("msg").setAttribute("class", "textareaMsgGreen")
+    document.getElementById("Input").setAttribute("class", "inputNameGreen")
+    document.getElementById("nChat").setAttribute("class", "chatGreen")
+}
+
+function backgroundFish() {
+    document.getElementById("full").setAttribute("class", "fullFish")
+}
+function backgroundSkull() {
+    document.getElementById("full").setAttribute("class", "fullSkull")
+}
+function backgroundFlower() {
+    document.getElementById("full").setAttribute("class", "fullFlower")
+}
+function alerta() {
+    alert("Nome: Lucas Lima do Nascimento \nMatrícula: 403010");
+}
+
+
